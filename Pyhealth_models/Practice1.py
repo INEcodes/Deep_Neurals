357ba6122ed421ccf6327b0b09238e3df936a739
Abhay Singh Chauhan <92663561+INEcodes@users.noreply.github.com>
2024-10-16 14:19:38 +0530

    Transformers and Recommendation

--- Pyhealth_models/Practice1.py
+++ Pyhealth_models/Practice1.py
@@ -9,3 +9,37 @@ mimic3base = MIMIC3Dataset(
 
 
 #step 2:
+from pyhealth.tasks import drug_recommendation_mimic3_fn
+from pyhealth.datasets import split_by_patient, get_dataloader
+
+mimic3sample = mimic3base.set_task(task_fn=drug_recommendation_mimic3_fn) # use default task
+train_ds, val_ds, test_ds = split_by_patient(mimic3sample, [0.8, 0.1, 0.1])
+
+# create dataloaders (torch.data.DataLoader)
+train_loader = get_dataloader(train_ds, batch_size=32, shuffle=True)
+val_loader = get_dataloader(val_ds, batch_size=32, shuffle=False)
+test_loader = get_dataloader(test_ds, batch_size=32, shuffle=False)
+
+#step 3:
+from pyhealth.models import Transformer
+
+model = Transformer(
+    dataset=mimic3sample,
+    feature_keys=["conditions", "procedures"],
+    label_key="drugs",
+    mode="multilabel",
+)
+
+#step 4:
+from pyhealth.trainer import Trainer
+
+trainer = Trainer(model=model)
+trainer.train(
+    train_dataloader=train_loader,
+    val_dataloader=val_loader,
+    epochs=50,
+    monitor="pr_auc_samples",
+)
+
+#step 5:
+trainer.evaluate(test_loader)
